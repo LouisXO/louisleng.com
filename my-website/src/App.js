dd45349789b1e6079b337e204a3203846d5f8a3d
LouisXO <qleng@uw.edu>
2022-07-12 15:36:09 -0700

    first commit

---
+++ my-website/src/App.js
@@ -0,0 +1,18 @@
+import babe from './Babe.PNG';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={babe} className="App-logo" alt="logo" />
+        <p>
+          赵海伦，我爱你哟！ - 乔乔
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
